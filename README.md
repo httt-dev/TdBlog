6dbcc90809f1a2bd458f8ea46d65441c9ea1f9c9
Hoa Nguyen <httt.dev@gmail.com>
2024-09-08 15:45:53 +0700

    Config migrate database

---
+++ README.md
@@ -0,0 +1,6 @@
+### Migration DB 
+- Selected TdBlog.Data project 
+- Open Package Manager Console : selected TdBlog.Data
+- Run cmd :
+	-  add-migration Initial	-  update-database
+
---
+++ src/TdBlog.Data/TbBlogContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TdBlog.Data
+{
+    public class TbBlogContextFactory : IDesignTimeDbContextFactory<TdBlogContext>
+    {
+        /// <summary>
+        /// Vi DI duoc khai bao tai project khac nen de co the migrate duoc thi can phai dung IDesignTimeDbContextFactory 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public TdBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<TdBlogContext>();
+
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new TdBlogContext(builder.Options);
+        }
+    }
+}
